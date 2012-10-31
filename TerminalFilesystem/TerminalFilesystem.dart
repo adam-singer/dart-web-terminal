228e55a496bd6e14ff0a996f904e74feb7d7c081
adam <financeCoding@gmail.com>
2012-10-30 20:46:51 -0700

    Totally screwed this checkin up, was on the wrong branch

--- TerminalFilesystem/TerminalFilesystem.dart
+++ TerminalFilesystem/TerminalFilesystem.dart
@@ -1,17 +1,52 @@
 #import('dart:core');
 #import('dart:html');
-//#import('dart:dom', prefix:"dom");
-//#import('dart:htmlimpl', prefix:"htmlimpl");
 #source('Terminal.dart');
-// #source('Commands.dart');
-class TerminalFilesystem {
 
+class TerminalFilesystem {
+  Terminal term;
   TerminalFilesystem() {
   }
 
   void run() {
-    Terminal t = new Terminal('#input-line', '#output', '#cmdline');
+    term = new Terminal('#input-line', '#output', '#cmdline');
+    term.initFS(false, 1024 * 1024);
     
+    print('window.location.hash = ${window.location.hash}');
+    if (!window.location.hash.isEmpty) {
+      var theme = window.location.hash.substring(1, window.location.hash.length).split('=')[1];
+      term.setTheme(theme);
+    } else if (window.localStorage.containsKey('theme')) {
+      term.setTheme(window.localStorage['theme']);
+    }
+    
+    // Setup the DnD listeners for file drop. 
+    var body = document.body;
+    body.on.dragEnter.add(_onDragEnter, false);
+    body.on.dragOver.add(_onDragOver, false);
+    body.on.drop.add(_onDrop, false);
+  }
+  
+  void _onDragEnter(MouseEvent event) {
+    event.stopPropagation();
+    event.preventDefault();
+    Element dropTarget = event.target;
+    dropTarget.classes.add('dropping');
+  }
+  
+  void _onDragOver(MouseEvent event) {
+    event.stopPropagation();
+    event.preventDefault();
+    // Explicitly show this is a copy.
+    event.dataTransfer.dropEffect = 'copy'; 
+  }
+  
+  void _onDrop(MouseEvent event) {
+    event.stopPropagation();
+    event.preventDefault();
+    Element dropTarget = event.target;
+    dropTarget.classes.remove('dropping');
+    term.addDroppedFiles(event.dataTransfer.files);
+    term.writeOutput('<div>File(s) added!</div>');
   }
 }
 
